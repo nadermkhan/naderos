0cf296621f6fba0189f986e72071710653775569
Your Name <your.email@example.com>
2025-06-21 20:55:13 +0600

    lokk last

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   //output: 'export',
+output: 'export',
  // basePath: '/naderos', 
   /* config options here */
     transpilePackages: ['onesignal'],
