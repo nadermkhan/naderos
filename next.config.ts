2270ba3805f294b6acfc90cf7bd37cd750b8215b
Your Name <your.email@example.com>
2025-06-21 21:02:40 +0600

    uploaddd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-output: 'export',
- // basePath: '/naderos', 
-  /* config options here */
-    transpilePackages: ['onesignal'],
+  output: 'export',
+  basePath: '/naderos',
+  assetPrefix: '/naderos',
+  transpilePackages: ['onesignal'],
 };
 
 export default nextConfig;
